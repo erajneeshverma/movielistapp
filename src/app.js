647a53b447b5fe0df2c674e37e758832e6b5742c
erajneeshverma <rajneeshverma94306@gmail.com>
2024-01-31 22:52:18 +0530

    Cros origin fixed

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://movielistapp-xi.vercel.app",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
